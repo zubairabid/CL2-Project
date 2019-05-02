bcf1387443ae80543245570e1aed4cd8f0dac1d3
zubairabid <zubairabid1999+github@gmail.com>
2019-05-02 15:38:57 +0530

    Cleaning script for newspaper data

--- datasets/news-indicnlp/News Articles_old/clean.py
+++ datasets/news-indicnlp/News Articles_old/clean.py
@@ -1,27 +1,32 @@
 from barely_json import parse
 
-print('Opening file to be cleaned')
-file1 = open('anandabazar_articles.txt')
-instr = file1.read()
 
-print('Starting the parse into dict')
-parsedict = parse(instr)
-print('Finished Parsing into the dict')
+filenames = ['anandabazar_articles', 'ebala_articles', 'zeenews_articles']
 
-c = 0
-outstr = '{\n'
+for filestr in filenames:
+	
+	print('Opening file {0} to be cleaned'.format(filestr))
+	file1 = open('../' + filestr + '.txt')
+	instr = file1.read()
 
-print('converting to JSONable format')
-for obj in parsedict['articles']:
-	outstr += '\'art'+str(c)+'\': '
-	outstr += str(obj)
-	outstr += ','
-	outstr += '\n'
-	c += 1
+	print('Starting the parse into dict')
+	parsedict = parse(instr)
+	print('Finished Parsing into the dict')
 
-outstr += '\n}'
+	c = 0
+	outstr = '{\n'
 
-file2 = open('anand.json', 'w')
-file2.write(outstr)
-print('written into anand.json')
+	print('converting to JSONable format')
+	for obj in parsedict['articles']:
+		outstr += '\'art'+str(c)+'\': '
+		outstr += str(obj)
+		outstr += ','
+		outstr += '\n'
+		c += 1
+
+	outstr += '\n}'
+
+	file2 = open(filestr + '.json', 'w')
+	file2.write(outstr)
+	print('written into {0}.json'.format(filestr))
 
